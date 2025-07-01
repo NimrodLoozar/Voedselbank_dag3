786b3d5e3e24988d54ce72e2ab8d9d85b4bbc65a
umvvi <wurmdragon1239@gmail.com>
2025-07-01 15:21:47 +0200

    everything done

--- resources/views/klanten/show.blade.php
+++ resources/views/klanten/show.blade.php
@@ -122,7 +122,7 @@
                                    class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md shadow-sm">
                                     terug
                                 </a>
-                                <a href="#" 
+                                <a href="{{ url('/') }}" 
                                    class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md shadow-sm">
                                     home
                                 </a>
