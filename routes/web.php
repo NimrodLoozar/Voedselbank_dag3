e121b2ed8c8b2f393ded1e1d0647bad6b7eafd0a
NimrodLobozar <nimrod.lobozar@gmail.com>
2025-07-02 12:34:37 +0200

    Refactor routes to consolidate inventory, klanten, voedselpakketten, leveranciers, producten, and magazijnen with new functionalities and API endpoints

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,34 @@ Route::get('/inventory/details/{product}', [App\Http\Controllers\ProductControll
 Route::get('/inventory/edit/{product}', [App\Http\Controllers\ProductController::class, 'editInventory'])->name('inventory.edit');
 Route::put('/inventory/update/{product}', [App\Http\Controllers\ProductController::class, 'updateInventory'])->name('inventory.update');
 
+// Klanten routes
+Route::get('/klanten', [KlantenController::class, 'index'])->name('klanten.index');
+Route::get('/klanten/{gezin}', [KlantenController::class, 'show'])->name('klanten.show');
+Route::get('/klanten/{gezin}/edit', [KlantenController::class, 'edit'])->name('klanten.edit');
+Route::put('/klanten/{gezin}', [KlantenController::class, 'update'])->name('klanten.update');
+
+Route::get('/voedselpakketten', [VoedselpakketController::class, 'index'])->name('voedselpakketten.index');
+Route::get('/voedselpakketten/show/{voedselpakket}', [VoedselpakketController::class, 'show'])->name('voedselpakketten.show');
+Route::get('/voedselpakketten/{voedselpakket}/edit', [VoedselpakketController::class, 'edit'])->name('voedselpakketten.edit');
+Route::post('/voedselpakketten', [VoedselpakketController::class, 'store'])->name('voedselpakketten.store');
+Route::patch('/voedselpakketten/{voedselpakket}', [VoedselpakketController::class, 'update'])->name('voedselpakketten.update');
+
+Route::resource('leveranciers', LeverancierController::class);
+
+// Voeg deze custom route toe voor product-edit via leverancier-edit view
+Route::get('/leveranciers/{leverancier}/product/{product}/edit', [LeverancierController::class, 'editProduct'])->name('leveranciers.product.edit');
+Route::put('/leveranciers/{leverancier}/product/{product}', [LeverancierController::class, 'updateProduct'])->name('leveranciers.product.update');
+
+// Volledige product resource routes
+Route::resource('producten', ProductController::class);
+
+// Magazijn routes met nieuwe functionaliteiten
+Route::resource('magazijnen', MagazijnController::class);
+Route::get('/magazijnen/statistics/get', [MagazijnController::class, 'getStatistics'])->name('magazijnen.statistics');
+
+// API routes voor AJAX calls
+Route::get('/api/expiring-products', [ProductController::class, 'getExpiringProducts'])->name('api.expiring-products');
+
 Route::get('/dashboard', function () {
     $isMaintenanceMode = Setting::isMaintenanceMode();
     return view('dashboard', compact('isMaintenanceMode'));
@@ -34,35 +62,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Klanten routes
-    Route::get('/klanten', [KlantenController::class, 'index'])->name('klanten.index');
-    Route::get('/klanten/{gezin}', [KlantenController::class, 'show'])->name('klanten.show');
-    Route::get('/klanten/{gezin}/edit', [KlantenController::class, 'edit'])->name('klanten.edit');
-    Route::put('/klanten/{gezin}', [KlantenController::class, 'update'])->name('klanten.update');
-
-    Route::get('/voedselpakketten', [VoedselpakketController::class, 'index'])->name('voedselpakketten.index');
-    Route::get('/voedselpakketten/show/{voedselpakket}', [VoedselpakketController::class, 'show'])->name('voedselpakketten.show');
-    Route::get('/voedselpakketten/{voedselpakket}/edit', [VoedselpakketController::class, 'edit'])->name('voedselpakketten.edit');
-    Route::post('/voedselpakketten', [VoedselpakketController::class, 'store'])->name('voedselpakketten.store');
-    Route::patch('/voedselpakketten/{voedselpakket}', [VoedselpakketController::class, 'update'])->name('voedselpakketten.update');
-
-
-    Route::resource('leveranciers', LeverancierController::class);
-
-    // Voeg deze custom route toe voor product-edit via leverancier-edit view
-    Route::get('/leveranciers/{leverancier}/product/{product}/edit', [LeverancierController::class, 'editProduct'])->name('leveranciers.product.edit');
-    Route::put('/leveranciers/{leverancier}/product/{product}', [LeverancierController::class, 'updateProduct'])->name('leveranciers.product.update');
-
-    // Volledige product resource routes
-    Route::resource('producten', ProductController::class);
-
-    // Magazijn routes met nieuwe functionaliteiten
-    Route::resource('magazijnen', MagazijnController::class);
-    Route::get('/magazijnen/statistics/get', [MagazijnController::class, 'getStatistics'])->name('magazijnen.statistics');
-
-    // API routes voor AJAX calls
-    Route::get('/api/expiring-products', [ProductController::class, 'getExpiringProducts'])->name('api.expiring-products');
 });
 
 Route::post('/toggle-maintenance', [MaintenanceController::class, 'toggle'])->name('toggle.maintenance');
